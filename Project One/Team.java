2335b45024bd987622d023ffdb8c21c7948572fb
Travis <71726772+Futrawei@users.noreply.github.com>
2022-05-17 15:15:10 -0400

    Update Team.java

--- Project One/Team.java
+++ Project One/Team.java
@@ -1,5 +1,9 @@
 package com.gamingroom;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * A simple class to hold information about a team
  * <p>
@@ -12,36 +16,56 @@ package com.gamingroom;
  * @author coce@snhu.edu
  *
  */
-// Team class extends Entity class.
 public class Team extends Entity{
-	long id;
-	String name;
+
+	/**
+	 * A list of the active teams
+	 */
+	private List<Player> players = new ArrayList<Player>();
 	
 	/*
 	 * Constructor with an identifier and name
 	 */
 	public Team(long id, String name) {
-		super();
-		this.id = id;
-		this.name = name;
+		super(id, name);
 	}
 
 	/**
-	 * @return the id
+	 * Uses iterator pattern to find existing player with same name or
+     * adds unique named player to list
+	 *
+	 * @param name
+	 * @return Player instance
 	 */
-	public long getId() {
-		return id;
-	}
+	public Player addPlayer(String name) {
 
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
+		// a local teams instance
+		Player player = null;
+
+		// Instance iterator
+		Iterator<Player> playersIterator = players.iterator();
+
+		// Iterate over players list
+		while (playersIterator.hasNext()) {
+
+			// Set local Player var to next item in list
+			Player playerInstance = playersIterator.next();
+
+			// Does player name already exist?
+			if (playerInstance.getName().equalsIgnoreCase(name)) {
+				// If player name already exists, return the player instance
+				player = playerInstance;
+			} else {
+				// else add the player to the players list
+				players.add(player);
+			}
+		}
+
+		return player;
 	}
 
 	@Override
 	public String toString() {
-		return "Team [id=" + id + ", name=" + name + "]";
+		return "Team [id=" + super.getId() + ", name=" + super.getName() + "]";
 	}
 }
