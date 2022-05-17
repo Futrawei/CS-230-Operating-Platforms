793efd979e6ef4643b12e61da003e486aa027dab
Travis <71726772+Futrawei@users.noreply.github.com>
2022-05-17 15:07:34 -0400

    Update Game.java

--- Project One/Game.java
+++ Project One/Game.java
@@ -1,8 +1,12 @@
 package com.gamingroom;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * A simple class to hold information about a game
- * 
+ *
  * <p>
  * Notice the overloaded constructor that requires
  * an id and name to be passed when creating.
@@ -10,47 +14,60 @@ package com.gamingroom;
  * these values cannot be changed once a game is
  * created.
  * </p>
- * 
+ *
  * @author coce@snhu.edu
  *
  */
-public class Game {
-	long id;
-	String name;
-	
+public class Game extends Entity {
+
 	/**
-	 * Hide the default constructor to prevent creating empty instances.
+	 * A list of the active teams
 	 */
-	private Game() {
-	}
+	private final List<Team> teams = new ArrayList<Team>();
 
 	/**
 	 * Constructor with an identifier and name
 	 */
 	public Game(long id, String name) {
-		this();
-		this.id = id;
-		this.name = name;
+		super(id, name);
 	}
 
 	/**
-	 * @return the id
+	 * Uses iterator pattern to find existing team with same name or
+	 * adds unique named team to list
+	 *
+	 * @return Team instance
 	 */
-	public long getId() {
-		return id;
-	}
+	public Team addTeam(String name) {
 
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
+		// a local team instance
+		Team team = null;
+
+		// Instance iterator
+		Iterator<Team> teamsIterator = teams.iterator();
+
+		// Iterate over teams list
+		while (teamsIterator.hasNext()) {
+
+			// Set local Team var to next item in list
+			Team teamInstance = teamsIterator.next();
+
+			// Does team name already exist?
+			if (teamInstance.getName().equalsIgnoreCase(name)) {
+				// If team name already exists, return the team instance
+				team = teamInstance;
+			} else {
+				// else add the team to the teams list
+				teams.add(team);
+			}
+		}
+
+		return team;
 	}
 
 	@Override
 	public String toString() {
-		
-		return "Game [id=" + id + ", name=" + name + "]";
+		return "Game [id=" + super.getId() + ", name=" + super.getName() + "]";
 	}
 
 }
